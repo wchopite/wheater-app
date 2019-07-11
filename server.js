a07363d11b6391698a99811b84a839caf17e83aa
Wladimir Chopite <wchopite@gmail.com>
2019-07-11 12:05:14 -0300

    server

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
+//Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
-const port = process.env.PORT || 8000;
-const server = require('http').Server(app);
 
-app.use(express.static(__dirname, 'dist', {index: false}));
+const app = express();
 
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/angular-weather'));
 
-server.listen(port, function() {
-    console.log("App running on port " + port);
-})
-
-// PathLocationStrategy
-
-app.get('', function(req, res) {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/dist/angular-weather/index.html'));
 });
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'));
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
